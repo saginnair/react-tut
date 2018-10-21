1f498fcd5e1921a185069d73fc55e1fff9dd8b2e
Sagin Nair <sagin.nair@jp.imshealth.com>
2018-10-21 15:31:20 +0900

    Adding a new component

--- react-redux-tut/myapp/src/App.js
+++ react-redux-tut/myapp/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Ninjas from './ninjas';
 //import logo from './logo.svg';
 //import './App.css';
 
@@ -6,20 +7,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img className="App-logo"/>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1> My first React App </h1>
+        <p> Welcome :) </p>
+        <Ninjas />
       </div>
     );
   }
---
+++ react-redux-tut/myapp/src/ninjas.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Ninjas extends Component {
+    render() {
+        return(
+            <div className="ninja">
+                <div>Name: Sagin</div>
+                <div>Age: 30</div>
+                <div>Belt: :)</div>
+            </div>
+        )
+    }
+}
+
+export default Ninjas
