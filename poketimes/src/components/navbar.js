76916c89720281b7fe1cff91a22c18c0962fbf26
Sagin Nair <sagin.nair@jp.imshealth.com>
2018-10-27 23:36:13 +0900

    Higher order components

--- poketimes/src/components/navbar.js
+++ poketimes/src/components/navbar.js
@@ -1,19 +1,20 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink, withRouter } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = (props) => {
+    console.log(props)
     return (
         <nav className="nav-wrapper red darken-3">
             <div className="container">
                 <a className="brand-logo">Poke Times</a>
                 <ul className="right">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/contact">Contact</a></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><NavLink to="/about">About</NavLink></li>
+                    <li><Link to="/contact">Contact</Link></li>
                 </ul>
             </div>
         </nav>
     )
 }
 
-export default Navbar
+export default withRouter(Navbar)
