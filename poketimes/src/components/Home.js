15e757b5789e2e0bfd2caf4294cdafb2795ff38d
Sagin Nair <sagin.nair@jp.imshealth.com>
2018-10-28 00:22:46 +0900

    Added support for Axios, Promises

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,12 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 
-const Home = () => {
-    return (
-        <div className = "container">
-            <h4 className="center">Home Page</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, voluptas neque excepturi eligendi dolor possimus deleniti, nisi doloribus aliquam eaque placeat rerum? Omnis accusantium neque architecto officiis ex placeat esse!</p>
-        </div>
-    )
+class Home extends Component {
+    state = {
+        posts : []
+    }
+    componentDidMount() {
+        axios.get("https://jsonplaceholder.typicode.com/posts")
+        .then(res => {
+            this.setState({
+                posts: res.data.slice(0,10)
+            })
+        })
+    }
+
+    render (){
+        const posts = this.state.posts;
+        const postslist = posts.length ? (
+            posts.map(post => {
+                return (
+                    <div className="post card" key={post.id}>
+                        <div className="card-content">
+                            <span className="card-title">
+                                {post.title}
+                            </span>
+                            <p>
+                                {post.body}
+                            </p>
+                        </div>
+                    </div>
+                )
+            })
+        ) : (
+            <div className="center">No posts as of yet</div>
+        )
+        return (
+            <div className = "container">
+                <h4 className="center">Home Page</h4>
+                {postslist}
+            </div>
+        )
+    }
 }
 
 export default Home
