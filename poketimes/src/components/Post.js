037849af011ae0a8a08b022697adb99ea067cb9c
Sagin Nair <sagin.nair@jp.imshealth.com>
2018-10-28 17:16:50 +0900

    Added display for individual post

--- poketimes/src/components/Post.js
+++ poketimes/src/components/Post.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class Post extends Component {
     state = {
-        id: null
+        post: null
     }
     componentDidMount() {
         let id = this.props.match.params.post_id
-        this.setState({
-            id: id
+        console.log (id)
+        axios.get('https://jsonplaceholder.typicode.com/posts/'+id)
+        .then(res => {
+            console.log(res.data)
+            this.setState({
+                post : res.data
+            })
         })
     }
     render() {
+        const post = this.state.post ? 
+            (
+                <div>
+                    <h4 className="center">{ this.state.post.title }</h4>
+                    <p>{ this.state.post.body }</p>
+                </div>
+            ) : 
+            (
+                <div className="center">
+                    <h4>Loadng post...</h4>
+                </div>
+            )
+        console.log({post})
         return (
             <div className="container">
-                <h4>{this.state.id}</h4>
+                <h4>{post}</h4>
             </div>
         )
     }
